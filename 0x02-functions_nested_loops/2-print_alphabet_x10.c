d24d5ec43018d369f0722013e59147d5b7efde1c
Ahmed <ahmedelsharkwiti@gmail.com>
2023-03-21 13:02:24 -0700

    mm

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print letters in lowercase ten times
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int o;
+
+	for (o = 0 ; o <= 10 ; o++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
