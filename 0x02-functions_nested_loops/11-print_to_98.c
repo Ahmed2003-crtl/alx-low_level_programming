a26dac9a29522ac961dd6bf8f4dfd083e7d1c0b7
Ahmed <ahmedelsharkwiti@gmail.com>
2023-03-22 10:57:58 -0700

    mm

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,11 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n ; i <= 98 ; i++)
+		for (i = n ; i < 98 ; i++)
 		{
 			printf("%d, ", i);
 		}
+		printf("%d", 98);
 		printf("\n");
 	}
 	else if (n == 98)
@@ -24,10 +25,11 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (j = n ; j >= 98 ; j--)
+		for (j = n ; j > 98 ; j--)
 		{
 			printf("%d, ", j);
 		}
+		printf("%d", 98);
 		printf("\n");
 	}
 }
