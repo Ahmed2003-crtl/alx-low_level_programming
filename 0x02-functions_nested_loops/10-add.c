94290a553bb80297df774c195bd0013b6c2fc176
Ahmed <ahmedelsharkwiti@gmail.com>
2023-03-22 10:52:25 -0700

    mm

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - calculate the result of addition
+ * @a: is an int
+ * @b: is an int
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
