3a14c71f39e6518e339f3f9ada526f3a9e09dbb8
Ahmed <ahmedelsharkwiti@gmail.com>
2023-03-27 08:41:29 -0700

    mm

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - print the str followed by a new line
+ * @str: is a pointer
+ * Return: 0 (success)
+ */
+void _puts(char *str)
+{
+	long unsigned int i;
+
+	for (i = 0 ; i < strlen(str) ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
