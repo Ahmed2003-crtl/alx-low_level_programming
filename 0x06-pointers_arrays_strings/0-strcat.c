12c7a2a457193e654523c725a6ac3c15b071180a
Ahmed <ahmedelsharkwiti@gmail.com>
2023-03-30 05:19:40 -0700

    mm

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - print two str
+ * @dest: is a pointer
+ * @src: is a pointer
+ * Return: 0 (success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	unsigned long int j;
+	int k = strlen(dest);
+
+	for (j = 0 ; j <= strlen(dest) ; j++)
+	{
+		dest[k] = src[j];
+		k++;
+	}
+	return (dest);
+}
