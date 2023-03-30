315308abbd4cbc34c54816ac52989356db6326d6
Ahmed <ahmedelsharkwiti@gmail.com>
2023-03-30 08:08:18 -0700

    mm

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * reverse_array - print the numbers of the numbers in reverse
+ * @a: is a pointer
+ * @n: is an int
+ * Return: 0 (success)
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int rev;
+	int k = 0;
+
+	for (i = n - 1 ; i >= 5 ; i--)
+	{
+		rev = a[i];
+		a[i] = a[k];
+		a[k] = rev;
+		k++;
+	}
+}
+
