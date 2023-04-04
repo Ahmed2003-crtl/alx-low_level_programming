f1dd25431b8c5284b15025c36b1a7de380c0a7f5
Ahmed <ahmedelsharkwiti@gmail.com>
2023-04-04 05:41:11 -0700

    mm

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  _strspn - print the number of that string
+ *  @s: is a pointer
+ *  @accept: is a pointer
+ *  Return: 0 (success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, l;
+	unsigned int rev = 0;
+
+	for (i = 0 ; accept[i] != '\0' ; i++)
+	{
+		for (l = 0 ; s[l] != '\0' ; l++)
+			if (s[l] == accept[i])
+			{
+				rev++;
+			}
+	}
+	return (strlen(s) - rev);
+}
