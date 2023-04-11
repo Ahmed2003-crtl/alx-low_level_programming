1170aefa43ae7377b55340d1cedd9081db28025d
Ahmed <ahmedelsharkwiti@gmail.com>
2023-04-11 15:31:11 -0700

    mm

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicate the str
+ * @str: is a str
+ * Return: 0 (success)
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *s = malloc(sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}
