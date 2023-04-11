e1f79d7146caef8686819afaa107b91eb297a945
Ahmed <ahmedelsharkwiti@gmail.com>
2023-04-11 04:48:00 -0700

    mm

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - check if the letter is exit in sentence
+ * @size: is an unsigned int
+ * @c: is a char
+ * Return: 0 (success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *s = malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+}
+
+
