5c0726d2fa43dca414796bbd919a5f1282b6aa11
Ahmed <ahmedelsharkwiti@gmail.com>
2023-04-14 06:19:43 -0700

    MM

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,18 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 1;
 
-	if (argc > 1)
+	if ( argc < 3 && argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 	{
 		for (i = 1 ; i < argc ; i++)
 		{
 			sum = sum * atoi(argv[i]);
 		}
 		printf("%d\n", sum);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 	return (0);
 }
