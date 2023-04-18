950b91afeb077fb65af6bf515442fad517a16117
Ahmed <ahmedelsharkwiti@gmail.com>
2023-04-18 05:59:55 -0700

    MM

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - is a struct
+ * @name: is a char
+ * @age: is a float
+ * @owner: is a char
+ *
+ *
+ * Description: it is a long struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
