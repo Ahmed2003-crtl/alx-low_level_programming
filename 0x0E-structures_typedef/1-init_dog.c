05651d9a6859cdbddb1e84e3cc68f1e39c343255
Ahmed <ahmedelsharkwiti@gmail.com>
2023-04-18 06:15:52 -0700

    MM

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - put values to arguments
+ * @d: is a pointer
+ * @name: is a pointer
+ * @age: is a pointer
+ * @owner: is a pointer
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
