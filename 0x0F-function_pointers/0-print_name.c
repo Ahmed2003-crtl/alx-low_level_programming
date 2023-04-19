536aaad8da935c4baefda3aa916e6dc7d46d11ba
Ahmed <ahmedelsharkwiti@gmail.com>
2023-04-19 07:15:03 -0700

    MM

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - print the name
+ * @f: is a pointer
+ * @name: is a char
+ * Return: 0 (success)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
