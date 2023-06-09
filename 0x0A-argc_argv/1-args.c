598b4ef0c830f3572cb94ae75a51bd21f844bc17
Your Name <you@example.com>
2023-06-09 12:39:10 -0700

    second commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the number of the line commands arguments
+ * @argc: is the number of line command argument
+ * @argv: is a pointer
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
