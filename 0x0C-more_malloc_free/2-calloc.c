631c1627f745199bfa19680749d9334f85c393e3
Your Name <you@example.com>
2023-06-15 07:37:25 -0700

    MM

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
 	p = malloc(nmemb * size);
 	if (p == NULL || nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
+	for (i = 0 ; i < nmemb ; i++)
+		p[i] = 0;
 	return (p);
 }
