5e9b5b8b154e89ea16886911dd2ccf6b75b9412c
Your Name <you@example.com>
2023-06-20 07:49:38 -0700

    the first commit in struct task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
