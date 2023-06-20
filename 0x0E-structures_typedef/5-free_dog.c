876f1f48f777aebb72ef9dff158da66e49f61005
Your Name <you@example.com>
2023-06-20 10:40:34 -0700

    the last commit in this task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free the space of struct my_dog
+ * @d: is a pointer which will hold the address of my_dog
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
