13afd70dc77975406509109563b5de1d1a032b3e
Your Name <you@example.com>
2023-06-22 11:14:26 -0700

    MM

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,7 +11,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list(p);
 	unsigned int i;
 	char *text;
-	char *te = "(nil)";
 
 	va_start(p, n);
 	if (separator == NULL)
@@ -23,9 +22,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		text = va_arg(p, char*);
 		if (*text == '\0')
 		{
-			text = te;
+			printf("(nil)");
 		}
-		printf("%s", text);
+		else
+			printf("%s", text);
 		if (i < n - 1)
 			printf("%s", separator);
 	}
