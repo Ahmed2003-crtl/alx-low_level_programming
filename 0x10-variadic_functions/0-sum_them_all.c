13a3289d9d2b707e4667e5b172a1599c36294ba9
Your Name <you@example.com>
2023-06-23 02:05:04 -0700

    MM

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,14 +7,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list(p);
 	unsigned int i;
 	int sum = 0;
+
 	if (n == 0)
 	{
 		return (0);
 	}
 
-	va_list(p);
 	va_start(p, n);
 	for (i = 0 ; i < n ; i++)
 	{
