e21283724c7887ccf2f326466ac5c1e8b3fef47f
Your Name <you@example.com>
2023-06-30 05:57:25 -0700

    MM

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ * print_list - print the elements of the list
+ * @h: is a pointer
+ * Return: 0 (success)
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0 ; h != NULL ; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%u] %s", h->len, h->str);
+		}
+		printf("\n");
+		h = h->next;
+	}
+	return (i);
+}
