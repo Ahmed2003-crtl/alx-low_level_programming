92dc608292a5ef01880c27f4f04f6f39da4039aa
Your Name <you@example.com>
2023-06-30 06:17:26 -0700

    MM

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,31 @@
-#include<stdio.h>
-#include"lists.h"
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 /**
- * print_list - print the elements of the list
+ * print_list - print the number of elementes
  * @h: is a pointer
  * Return: 0 (success)
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t j = 0;
 
-	for (i = 0 ; h != NULL ; i++)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] ");
+			printf("(nil)");
 		}
 		else
 		{
-			printf("[%u] %s", h->len, h->str);
+			printf("[%u] ", h->len);
+			printf("%s", h->str);
 		}
+		j++;
 		printf("\n");
 		h = h->next;
 	}
-	return (i);
+	return (j);
 }
