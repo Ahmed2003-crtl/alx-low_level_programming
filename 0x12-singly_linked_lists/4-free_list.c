23653bd022d2b8fa0e6753794b879e6eb59af976
Your Name <you@example.com>
2023-06-30 12:42:13 -0700

    MM

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"lists.h"
+/**
+ * free_list - it will free the list
+ * @head: is a pointer
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	while (temp != NULL)
+	{
+		head = head->next;
+		free(temp);
+		temp = head;
+	}
+}
