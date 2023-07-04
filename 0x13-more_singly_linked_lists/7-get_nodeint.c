49de7b28633f2e25220bff930542064c72278431
Your Name <you@example.com>
2023-07-04 09:31:55 -0700

    MM

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *temp = head;
 
+	if (head == NULL)
+		return (NULL);
 	for (i = 0 ; i < index ; i++)
 		temp = temp->next;
-	if (temp == NULL)
-		return (NULL);
-
 	return (temp);
 }
