227a1cbbebf7f62fce1a404391993b89e67ece83
Your Name <you@example.com>
2023-07-04 07:33:22 -0700

    MM

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
+	if (head == NULL)
+		return;
+	temp = *head;
 	while (temp != NULL)
 	{
 		*head = temp->next;
 		free(temp);
 		temp = *head;
 	}
-	head = NULL;
+	*head = NULL;
 }
