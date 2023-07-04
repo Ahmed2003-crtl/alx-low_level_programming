1e6cfbbfaf5aa1535365d073f2b3937f0a7ad1d4
Your Name <you@example.com>
2023-07-04 07:03:03 -0700

    MM

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include"lists.h"
+#include<stdlib.h>
+/**
+ * add_nodeint_end - add a new node a the end of the list
+ * @head: is a pointer
+ * @n: is an int
+ * Return: 0 (success)
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp = malloc(sizeof(listint_t));
+	listint_t *key;
+
+	temp->n = n;
+	if (temp == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		temp->next = NULL;
+		*head = temp;
+		return (temp);
+	}
+	key = *head;
+	while (key->next != NULL)
+	{
+		key = key->next;
+	}
+	temp->next = key->next;
+	key->next = temp;
+	return (temp);
+}
