ff76864cdb1607640199ca8b910e2e20dfd47a2c
Your Name <you@example.com>
2023-07-04 09:28:15 -0700

    MM

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ * get_nodeint_at_index - get the element which is exist inside the node
+ * @head: is a pointer
+ * @index: is an int
+ * Return: 0 (success)
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp = head;
+
+	for (i = 0 ; i < index ; i++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
+
+	return (temp);
+}
