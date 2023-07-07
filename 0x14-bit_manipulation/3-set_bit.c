f145dd0e1a06d245cab8412e5bd1438146685ffb
Your Name <you@example.com>
2023-07-07 11:32:00 -0700

    MM

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,36 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"main.h"
-int len(int u);
+#include "main.h"
+
 /**
- * set_bit -  sets the value of a bit to 1 at a given index
- * @n: is a pointer
- * @index: is an int
- * Return: 0 (success)
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the number to change
+ * @index: index of the bit to set to 1
+ *
+ * Return: 1 for success, -1 for failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int i, j, l = len(*n);
-        unsigned int count = 0;
+	if (index > 63)
+		return (-1);
 
-        if (n == NULL)
-                return (-1);
-        for (i = l - 1 ; i >= 0 ; i--)
-        {
-                n[i] = n % 2;
-                n = n / 2;
-        }
-        for (j = 0 ; j < l ; j++)
-        {
-                if (count == index)
-                        p[i] = 1;
-                count++;
-        }
-
-        return (-1);
+	*n = ((1UL << index) | *n);
+	return (1);
 }
-/**
- * len - calculate the len of any number
- * @u: is an int
- * Return: 0 (success)
- */	
