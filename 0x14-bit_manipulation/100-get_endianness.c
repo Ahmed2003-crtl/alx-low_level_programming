b7a69e5d3178a3abdcb80a99491e891a35e460f0
Your Name <you@example.com>
2023-07-07 12:39:57 -0700

    MM

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * get_endianness - check what is the endianness of your device
+ * Return: 0 (success)
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *s = (char*)&i;
+
+	if (*s)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
