e3072d8e81adc719f1a4068ceb35c7ceae863717
Your Name <you@example.com>
2023-07-10 13:34:46 -0700

    MM

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,7 @@ int create_file(const char *filename, char *text_content)
 	int fp;
 	int w;
 
-	if (access(filename, F_OK) == -1)
-		chmod(filename, 0600);
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fp, text_content, strlen(text_content));
 	if (fp == -1 || filename == NULL || text_content == NULL || w == -1)
 		return (-1);
