2331536da9f4ec608ff38b68741a30e974220c7c
Your Name <you@example.com>
2023-07-10 13:45:19 -0700

    MM

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content)
+	if (text_content == NULL)
 		len = 0;
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fp, text_content, len);
